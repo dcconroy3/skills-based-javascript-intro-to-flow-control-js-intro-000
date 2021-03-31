ac73f2a84670a0e88d315d83159eabf62521f9db
daniel conroy <dcconroy3@gmail.com>
2021-03-31 01:31:08 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
-
+  if (age >=13 && age <=19) {
+    return `"You are a teenager!"`
+  } else {
+    return `"undefined"` }
 }
+basicTeenager(13) + basicTeenager(12)
 
 function teenager(age) {
-
+  if (age >=13 && age <=19) {
+    return `"You are a teenager!"`
+} else {
+  return `"You are not a teenager!"` }
 }
+teenager(13) + teenager(12) + teenager(29)
 
 function ageChecker(age) {
-
-}
+  if (age >=13 && age <= 19) {
+    return `"You are a teenager!"`
+  } else if (age <= 12) {
+    return `"You are a kid"`
+  } else (age >= 19) {
+    return `"You are a grownup"`}
+  }
+  ageChecker(13) + ageChecker(12) + ageChecker(29)
 
 function ternaryTeenager(age) {
-
+  return (age >=13 && age <= 19) ? `"You are a teenager!"` : `"You are not a teenager"`
 }
+ternaryTeenager(15) + ternaryTeenager(75)
 
 function switchAge(age) {
-
+  switch (age) {
+    case (age >=13 && age <= 19) :
+        return `"You are a teenager!"`
+        break;
+    default:
+        return `"You have an age"`
+      }
 }
